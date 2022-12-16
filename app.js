74bacfc9d1e26d060ac0323350adc24498add098
Elodie B <billelodie@gmail.com>
2022-12-16 13:26:49 +0100

    initialisation du serveur

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+mongoose.connect('mongodb+srv:arthur:<BTxkMmDlXUOMbk6I>@cluster0-pme76.mongodb.net / test ? retryWrites = true & w=majority',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
+
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !')); 
+
+
+
+
+
+
+module.export = app;
