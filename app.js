7b39e3179d54006355e0f5dd590427639537cda3
Elodie B <billelodie@gmail.com>
2022-12-21 17:07:15 +0100

    installation mongoose, express, server fontionnel + variable environnement

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-mongoose.connect('mongodb+srv:arthur:<BTxkMmDlXUOMbk6I>@cluster0-pme76.mongodb.net / test ? retryWrites = true & w=majority',
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true})
+const dotenv = require('dotenv');
+const dotenvResult = dotenv.config();
 
+const mongooseUrlConnect =  process.env.DB_URL;
+mongoose.connect( `${mongooseUrlConnect}`,
+{useNewUrlParser: true,
+    useUnifiedTopology: true})
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !')); 
+console.log(process.env.MONGOOSE_URL);
 
 
 
 
 
-
-module.export = app;
+module.exports = app;
