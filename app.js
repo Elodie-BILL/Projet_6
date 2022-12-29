7c5b92bc498cf0d3e00802858d727afd57ae3382
Elodie B <billelodie@gmail.com>
2022-12-29 15:55:21 +0100

    création compte utiisateur non fonctionnel

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const dotenv = require('dotenv');
 dotenv.config();
 
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', false); // true: garantit que les valeurs transmises à notre constructeur de modèle qui n'ont pas été spécifiées dans notre schéma ne sont pas enregistrées dans la base de données.
+mongoose.set('strictQuery', true); //garantit que les valeurs transmises à notre constructeur de modèle qui n'ont pas été spécifiées dans notre schéma ne sont pas enregistrées dans la base de données.
 const mongooseUrlConnect =  process.env.DB_URL;
-
 // Connection mongoDB
 mongoose.connect( `${mongooseUrlConnect}`, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !')); 
 
-const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
--- controllers/user.js
+++ controllers/user.js
@@ -2,15 +2,17 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+
 //cryptage du mot de passe
 exports.signup = (req, res, next) => {
+    
     bcrypt.hash (req.body.password, 10)
     .then (hash =>{
         const user = new User({
             email: req.body.email,
             password: hash 
         });
-        user.save() 
+        user.save()
         .then(() => 
             res.status(201).json({ message: 'Compte utilisateur crée'})
         ) /*201 création de ressource*/
