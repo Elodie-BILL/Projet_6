13f6330b36bbee1896c55d9b5d91235f4d480177
Elodie B <billelodie@gmail.com>
2022-12-30 17:31:36 +0100

    prublication sauce invalide

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,17 +1,19 @@
 const sauceCrtl = require('../models/sauce');
 
 exports.createSauce = (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-      ...req.body
-    });
-    sauce.save()
-    .then(()=> {
-      res.status(201).json({message: 'Sauce ajoutée'}
-    )})
-    .catch ((error) => {
-      res.satus(400).json({ error });
-    });
+  const sauceObject = JSON.parse(raq.body.sauce);
+  delete sauceObject._id;
+  delete sauceObject._userId;
+  // récupération du token
+  const sauce = new Sauce({
+    ...sauceObject,
+    userId: req.auth.userId,
+    imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+
+  sauce.save()
+  .then(() => (res.status(201).json({message: 'Sauce enregistrée'})))
+  .cathc(error => { res.status(400).json({error})});
  
 }
 
--- routes/sauce.js
+++ routes/sauce.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 
 const sauceCtrl = require('../controllers/sauce');
@@ -8,7 +9,7 @@ const sauceCtrl = require('../controllers/sauce');
 
 
 router.get('/', auth,sauceCtrl.getAllSauce);
-router.post('/', auth, sauceCtrl.createSauce);
+router.post('/', auth,multer, sauceCtrl.createSauce);
 // router.post('/:id/like', auth, sauceCtrl.likeSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, sauceCtrl.modifySauce);
